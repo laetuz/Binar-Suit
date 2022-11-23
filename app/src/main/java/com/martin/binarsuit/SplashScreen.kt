56c43a7ed5f4e1e9853adca8729d62c53c8fb050
Ryo Martin <laetuzg@gmail.com>
2022-11-24 03:17:08 +0700

    Disabled super.onBackPressed for back button.

--- app/src/main/java/com/martin/binarsuit/SplashScreen.kt
+++ app/src/main/java/com/martin/binarsuit/SplashScreen.kt
@@ -1,5 +1,6 @@
 package com.martin.binarsuit
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,9 +12,14 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+
         Handler(Looper.getMainLooper()).postDelayed({
             Intent(this@SplashScreen, MainActivity::class.java).also { startActivity(it) }
             finish()
         }, 1000)
+    }
+
+    override fun onBackPressed() {
+       // super.onBackPressed()
     }
 }
