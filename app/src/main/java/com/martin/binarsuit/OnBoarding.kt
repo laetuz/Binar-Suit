397d5a1a465802f47fe91f6166fb7ecdea051046
Ryo Martin <laetuzg@gmail.com>
2022-11-26 06:32:26 +0700

    Cleaning codes and imported dialog screen.

--- app/src/main/java/com/martin/binarsuit/OnBoarding.kt
+++ app/src/main/java/com/martin/binarsuit/OnBoarding.kt
@@ -1,14 +1,19 @@
 package com.martin.binarsuit
 
+import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.martin.binarsuit.databinding.ActivityOnBoardingBinding
+import com.martin.binarsuit.databinding.DialogViewBinding
 
 class OnBoarding : AppCompatActivity() {
     private lateinit var binding: ActivityOnBoardingBinding
+    private lateinit var bindingDialog: DialogViewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
         binding = ActivityOnBoardingBinding.inflate(layoutInflater)
         val view = binding.root
@@ -18,5 +23,20 @@ class OnBoarding : AppCompatActivity() {
             Intent(this, MainActivity::class.java).also { startActivity(it) }
                 .also { finishAffinity() }
         }
+    }
+
+    //Display dialog. Menampilkan Dialog.
+    override fun onBackPressed() {
+        bindingDialog = DialogViewBinding.inflate(layoutInflater)
+        val view = bindingDialog.root
+        val builder = AlertDialog.Builder(this@OnBoarding)
+        builder.setView(view)
+
+        val dialog = builder.create()
+        dialog.show()
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+        bindingDialog.btnBack.setOnClickListener { dialog.hide() }
+        bindingDialog.btnExit.setOnClickListener { finish() }
     }
 }
