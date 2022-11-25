37674ef76f6dc4443a1c8612cc89100d2269ef2d
Ryo Martin <laetuzg@gmail.com>
2022-11-25 23:14:15 +0700

    Added animation on splash screen.

--- app/src/main/java/com/martin/binarsuit/SplashScreen.kt
+++ app/src/main/java/com/martin/binarsuit/SplashScreen.kt
@@ -6,18 +6,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.ImageView
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        val spLogo:ImageView=findViewById(R.id.iv_logo)
+        spLogo.animate().apply {
+            duration = 700
+            rotationBy(360f)
+        }.start()
 
         Handler(Looper.getMainLooper()).postDelayed({
-            Intent(this@SplashScreen, MainActivity::class.java).also { startActivity(it) }
+            Intent(this@SplashScreen, OnBoarding::class.java).also { startActivity(it) }
             finish()
         }, 1000)
     }
+
 
     override fun onBackPressed() {
        // super.onBackPressed()
